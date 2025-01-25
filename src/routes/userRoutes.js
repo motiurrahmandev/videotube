4e40a9bcb7d0b78622be28e571fad2596c3d8ffa
MotiurRahman <rmdmotiur144@gmail.com>
2025-01-25 22:22:51 +0600

    add register controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,21 @@
-import {Router } from 'express'
-import { registerUser } from '../controllers/userController.js'
+import { Router } from "express";
+import { registerUser } from "../controllers/userController.js";
+import { upload } from "../middlewares/multerMiddleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      macCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-export default router
+export default router;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,6 +25,10 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
+export {
+  uploadOnCloudinary
+}
+
 /*
 
 (async function() {
